4a0e9914c8cc12bb049b64ab9c87ad57616b9e5e
Rishav-R03 <rishav042023@gmail.com>
2024-12-22 23:38:58 +0530

    adding new schemas

--- schemas.py
+++ schemas.py
@@ -8,3 +8,10 @@ class user(BaseModel):
 class loginReq(BaseModel):
     email:str
     password:str
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str
+
+class TokenData(BaseModel):
+    username:str | None = None

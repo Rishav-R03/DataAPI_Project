466bd69b07b422f5ee9faf1d7fc9472171a31405
Rishav-R03 <rishav042023@gmail.com>
2024-12-23 14:21:27 +0530

    added table model to store csv to database

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,Float,Boolean
 from database import Base
 import datetime
 
-class User(Base):
+class User(Base): # for register users
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(100), nullable=False)
@@ -10,3 +10,16 @@ class User(Base):
     password = Column(String(100), nullable=False)
     created_at = Column(String, default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     updated_at = Column(String, default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+class CSVData(Base):
+    __tablename__ = "csv_data"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)  # Ensure these fields exist and match
+    age = Column(Integer, nullable=False)
+    occupation = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    salary = Column(Float, nullable=False)
+    married = Column(Boolean, nullable=False)
+
+
